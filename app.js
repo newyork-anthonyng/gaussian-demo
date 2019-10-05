f5fe3c66224d1a5da29aaa1c255d5fb900e05211
Anthony Ng <newyork.anthonyng@gmail.com>
2019-10-05 11:46:22 -0400

    Add keyboard support

--- app.js
+++ app.js
@@ -12,13 +12,7 @@ document.addEventListener('mousemove', function(e) {
         return;
     }
 
-    const xPosition = Math.max(0, Math.min(e.clientX, $trackContainer.clientWidth - $trackButton.clientWidth));
-
-    const xPercentage = xPosition / ($trackContainer.clientWidth - $trackButton.clientWidth);
-    const normalizedX = window.getNormalizedX(xPercentage);
-    const yPercentage = window.normalDistribution(normalizedX);
-    const yPosition = $trackContainer.clientHeight - ($trackContainer.clientHeight * yPercentage) - $trackButton.clientHeight;
-
+    const { xPosition, yPosition } = getXAndYPosition(e.clientX);
     $trackButton.style.left = `${xPosition}px`;
     $trackButton.style.top = `${yPosition}px`;
 });
@@ -26,3 +20,38 @@ document.addEventListener('mousemove', function(e) {
 document.addEventListener('mouseup', function() {
     isDragging = false;
 });
+
+$trackButton.addEventListener('keydown', function(e) {
+    const keyCode = e.key;
+    let direction = 0;
+    if (keyCode === 'ArrowRight') {
+        direction = 1;
+    } else if (keyCode === 'ArrowLeft') {
+        direction = -1;
+    } else {
+        return;
+    }
+
+    const STEP_INCREMENT = 10;
+    const step = direction * STEP_INCREMENT;
+    const originalX = $trackButton.style.left ?
+        +$trackButton.style.left.substring(0, $trackButton.style.left.length - 2) :
+        0.1;
+    const { xPosition, yPosition } = getXAndYPosition(originalX + step);
+
+    $trackButton.style.left = `${xPosition}px`;
+    $trackButton.style.top = `${yPosition}px`;
+});
+
+function getXAndYPosition(rawXPosition) {
+    const xPosition = Math.max(0, Math.min(rawXPosition, $trackContainer.clientWidth - $trackButton.clientWidth));
+    const xPercentage = xPosition / ($trackContainer.clientWidth - $trackButton.clientWidth);
+    const normalizedX = window.getNormalizedX(xPercentage);
+    const yPercentage = window.normalDistribution(normalizedX);
+    const yPosition = $trackContainer.clientHeight - ($trackContainer.clientHeight * yPercentage) - $trackButton.clientHeight;
+
+    return {
+        xPosition,
+        yPosition
+    };
+}
